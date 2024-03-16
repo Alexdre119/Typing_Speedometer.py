cf7eef6e699ac678694f3da766e061f9509bdd78
Alexdre119 <alex.darger@icloud.com>
2024-03-16 16:53:51 -0600

    Update. Dial not working.

--- main.py
+++ main.py
@@ -41,26 +41,25 @@ class SpeedometerApp:
 
     def draw_speedometer(self):
         # Draw circle
-        self.canvas.create_arc(50, 50, 250, 250, start=180, extent=180, outline="black", width=2)
+        self.canvas.create_arc(50, 50, 250, 250, start=0, extent=180, outline="black", width=2)
 
         # Draw markings
-        for angle in range(180, 0, -10):
+        for angle in range(0, 181, 20):
             x1 = 150 + 100 * cos(angle * pi / 180)
-            y1 = 150 + 100 * sin(angle * pi / 180)
+            y1 = 150 - 100 * sin(angle * pi / 180)  # Flipped y-coordinate
             x2 = 150 + 90 * cos(angle * pi / 180)
-            y2 = 150 + 90 * sin(angle * pi / 180)
+            y2 = 150 - 90 * sin(angle * pi / 180)  # Flipped y-coordinate
             self.canvas.create_line(x1, y1, x2, y2, fill="black")
 
             # Add numbers
-            if angle % 20 == 0:
-                x_text = 150 + 110 * cos(angle * pi / 180)
-                y_text = 150 + 110 * sin(angle * pi / 180)
-                self.canvas.create_text(x_text, y_text, text=str(180 - angle), fill="black")
+            x_text = 150 + 110 * cos(angle * pi / 180)
+            y_text = 150 - 110 * sin(angle * pi / 180)  # Flipped y-coordinate
+            self.canvas.create_text(x_text, y_text, text=str(angle), fill="black")
 
     def update_dial(self, cpm):
-        angle = 180 - min(cpm / 10, 100) * 1.8  # 1.8 degrees per CPM
+        angle = cpm * 1.8  # 1.8 degrees per CPM
         self.canvas.delete("dial")  # Delete previous dial
-        self.canvas.create_line(150, 150, 150 + 100 * cos(angle * pi / 180), 150 + 100 * sin(angle * pi / 180),
+        self.canvas.create_line(150, 150, 150 + 100 * cos(angle * pi / 180), 150 - 100 * sin(angle * pi / 180),
                                 fill="red", width=2, tags="dial")
 
 # Create the Tkinter application
